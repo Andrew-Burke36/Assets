cfee6eae83eb978329fde8aed6a5b98d1de75b91
Andrew John Burke <s10266774@connect.np.edu.sg>
2025-06-10 15:33:14 +0800

    Highlight materials
    Objects now interacted with or if the player is within the trigger will now highlight.

--- Scripts/CoinBehaviour.cs
+++ Scripts/CoinBehaviour.cs
@@ -3,19 +3,54 @@ using UnityEngine;
 public class CoinBehaviour : MonoBehaviour
 {
     int CoinValue = 1;
-    public void CollectCoin(PlayerBehaviour player) {
+
+    [SerializeField]
+    AudioClip coinCollectAudio;
+    MeshRenderer myMeshRenderer;
+
+    [SerializeField]
+    Material highlightMat;
+
+    Material originalMat;
+
+    public void CollectCoin(PlayerBehaviour player)
+    {
+        // Play audio sound
+        AudioSource.PlayClipAtPoint(coinCollectAudio, transform.position);
+
+        // Increases the players score by 1
         player.ModifyScore(CoinValue);
+
+        // Destroys the coin
         Destroy(gameObject);
+    }
+
+    public void Highlight()
+    {
+        // Changes the coin to the highlight colour 
+        myMeshRenderer.material = highlightMat;
+    }
+
+    public void Unhighlight()
+    {
+        // Changes the color of the coin to its original colour
+        myMeshRenderer.material = originalMat;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        // Gets the mesh renderer of the coin
+        myMeshRenderer = GetComponent<MeshRenderer>();
+
+        // Stores the original colour of the coin
+        originalMat = myMeshRenderer.material;      
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Gives the coin a rotating animation
         transform.Rotate(0,0.2f,0);
     }
 }
--- Scripts/PlayerBehaviour.cs
+++ Scripts/PlayerBehaviour.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
 
 public class PlayerBehaviour : MonoBehaviour
 {
@@ -13,9 +14,10 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField]
     float fireStrength = 100f;
 
-    private int matchaHealth = 100;
-    private int maxMatchaHealth = 100;
-    int score = 0;
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+
+    int currentScore = 0;
     
     bool canInteract = false;
     CoinBehaviour currentCoin;
@@ -28,7 +30,7 @@ public class PlayerBehaviour : MonoBehaviour
             if (currentCoin != null) {
                 Debug.Log("Collecting..");
                 currentCoin.CollectCoin(this);
-                Debug.Log("Score: " + score);
+                Debug.Log("Score: " + currentScore);
                 canInteract = false;
                 currentCoin = null;
             }
@@ -43,55 +45,39 @@ public class PlayerBehaviour : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Player collided with: " + other.gameObject.name);
-        if(other.CompareTag("Coin")) {
+        if (other.CompareTag("Coin"))
+        {
             canInteract = true;
             currentCoin = other.gameObject.GetComponent<CoinBehaviour>();
+            currentCoin.Highlight();
         }
-        else if(other.CompareTag("KeycardDoor")) {
+        else if (other.CompareTag("KeycardDoor"))
+        {
             canInteract = true;
             currentDoor = other.gameObject.GetComponent<DoorBehaviour>();
         }
-
-
-    }
-    public void ModifyScore(int amount) {
-        score += amount;
     }
 
-    // void OnCollisionEnter(Collision collision)
-    // {
+    void OnTriggerExit(Collider other)
+    {
+        // Check if the player has detected a coin 
+        if (currentCoin != null)
+        {
+            // If the object that exited the trigger is the same as the current coin
+            if (other.gameObject == currentCoin.gameObject)
+            {
+                canInteract = false;
+                currentCoin.Unhighlight();
+                currentCoin = null;
+            }
+        }
+    }
+    public void ModifyScore(int amount)
+    {
+        currentScore += amount;
+        scoreText.text = "SCORE: " + currentScore.ToString();
 
-    //     if (collision.gameObject.CompareTag("matcha"))
-    //     {
-    //         matchaHealth++;
-    //         Debug.Log("Matcha health: " + matchaHealth);
-    //     }
-    //     else if (collision.gameObject.CompareTag("coffee")){
-    //         matchaHealth--;
-    //         Debug.Log("Matcha health: " + matchaHealth);
-    //     }
-    //     else {
-    //         Debug.Log("Player collided with a non-matcha object");
-    //     }
-    // }
-
-    // void OnCollisionEnter(Collision collision) {
-    //     if (collision.gameObject.CompareTag("matcha_boost")) {
-    //         Debug.Log("player collided with a matcha boost object");
-
-    //         RecoveryBehaviour recover = collision.gameObject.GetComponent<RecoveryBehaviour>();
-    //     }
-    // }
-
-    // public void ModifyHealth(int amount) {
-    //     if(matchaHealth < maxMatchaHealth) {
-    //         matchaHealth += amount;
-    //         if (matchaHealth > maxMatchaHealth) {
-
-    //             matchaHealth = maxMatchaHealth;
-    //         }
-    //     } 
-    // }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnFire()
@@ -107,7 +93,7 @@ public class PlayerBehaviour : MonoBehaviour
     }
     void Start()
     {
-        
+        scoreText.text = "SCORE: " + currentScore.ToString();
     }
 
     // Update is called once per frame
